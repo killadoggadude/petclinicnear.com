9cd3a1449a2daca6c69c6e32f6187957e9498887
killadoggadude <tobiasfendt87@googlemail.com>
2025-04-12 07:47:15 +0800

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="icon" href="/images/favicon.png" />
+        </Head>
         <body>
           <Main />
           <NextScript />
